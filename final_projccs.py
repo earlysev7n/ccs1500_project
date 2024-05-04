7e6dc58cf7ef58cf95a50c282a01c6f5f413a683
earlysev7n <111173265+earlysev7n@users.noreply.github.com>
2024-05-04 19:09:33 +0800

    Update final_projccs.py

---
+++ final_projccs.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, username):
+        self.username = username
+
+class Product:
+
+class Seller(User):
+
+class Customer (User):
+
+class Order:
+    
+
+class Payment:
+  
