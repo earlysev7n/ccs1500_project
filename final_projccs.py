7061c5ccc4e9ed3ff7d0151203f65e63920c0124
earlysev7n <111173265+earlysev7n@users.noreply.github.com>
2024-05-04 19:51:37 +0800

    Update final_projccs.py

--- final_projccs.py
+++ final_projccs.py
@@ -3,13 +3,24 @@ class User:
         self.username = username
 
 class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 class Seller(User):
+#call the parent class using super()
+#add products 
 
 class Customer (User):
+#call the parent class using super()
+#add to cart
+#calculate the total
 
 class Order:
-    
+    def __init__(self, customer, products):
+        self.customer = customer
+        self.products = products
 
 class Payment:
+#checkout print the total amount and ask for an input if the user wants to buy the products add a yes/no question
   
